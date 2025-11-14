9b60e9776480b7dd85724de3065f565fe720a2d3
anotherSimpleCoder <71701743+anotherSimpleCoder@users.noreply.github.com>
2025-11-14 12:10:06 +0100

    Refactor SSD1306 I2C class methods

--- SSD1306/src/SSD1306_I2C.cc
+++ SSD1306/src/SSD1306_I2C.cc
@@ -1,23 +1,32 @@
-#ifndef SSD1306_I2C_HH
-#define SSD1306_I2C_HH
-
+//
+// Created by amsakan on 03.11.25.
+//
 #include "stm32f1xx_hal.h"
+#include "SSD1306_I2C.hh"
 
-namespace SSD1306 {
-    class I2C {
-        I2C_HandleTypeDef* i2c_handle;
-    public:
-        const uint8_t address = 0x78;
-        const unsigned int timeout = 20000;
+void SSD1306::I2C::init() {
+    uint32_t p = 250000;
+    while(p>0)
+        p--;
+}
 
-        explicit I2C(I2C_HandleTypeDef* i2c_handle)
-            :i2c_handle(i2c_handle) {}
 
-        void init();
-        void write(uint8_t address, uint8_t  reg, uint8_t  data) const;
-        void write_multi(uint8_t address, uint8_t reg, uint8_t *data, uint16_t count) const;
-        I2C_HandleTypeDef* get_i2c_handle() const;
-    };
-};
+void SSD1306::I2C::write(uint8_t address, uint8_t reg, uint8_t data) const {
+    uint8_t dt[2];
+    dt[0] = reg;
+    dt[1] = data;
+    HAL_I2C_Master_Transmit(i2c_handle, address, dt, 2, 10);
+}
 
-#endif
+void SSD1306::I2C::write_multi(uint8_t address, uint8_t reg, uint8_t *data, uint16_t count) const {
+    uint8_t dt[256];
+    dt[0] = reg;
+    uint8_t i;
+    for(i = 0; i < count; i++)
+        dt[i+1] = data[i];
+    HAL_I2C_Master_Transmit(i2c_handle, address, dt, count+1, 10);
+}
+
+I2C_HandleTypeDef* SSD1306::I2C::get_i2c_handle() const {
+    return this->i2c_handle;
+}
